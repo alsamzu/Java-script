5cc56dd90b08ccdca2d2fdfa56b57e8364530628
mohammed yusuf <me@mwaigalo5.com>
2018-04-06 16:07:44 +0300

    jav

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+////////document.write("Hello world");
+////////console.log("Day one");
+////////alert("test");
+//////
+//////
+//////var x="5";
+//////var X="7";
+//////parseInt(x);
+//////console.log(x);
+//////console.log(X);
+////var x=prompt("please enter your name");
+////alert(x);
+//var x=true;
+//console.log(x);
+var x=5;
+//var x==5;
+//var x===5;
+var y=7;
+var z=x+y;
+document.write("the sum of "  + x + " and " + y + " is " + z);
